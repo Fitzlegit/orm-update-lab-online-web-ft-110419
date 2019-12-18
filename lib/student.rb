7bbe8fe005abc3f2270dc1bd1e4ccaef91c04bfb
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-18 05:32:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,9 +13,9 @@ class Student
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
-        id INT PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         name TEXT,
-        grade TEXT
+        grade INTEGER
       )
     SQL
 
