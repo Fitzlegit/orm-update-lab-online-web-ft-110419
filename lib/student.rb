b9200afc5f66f89e3caab97b1b9ca91bc9b5a933
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-18 05:27:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,10 +47,11 @@ class Student
   end
 
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
+    new_stu = self.new
+    new_stu.id = row[0]
+    new_stu.name = row[1]
+    new_stu.grade = row[2]
+    new_stu
   end
 
   def self.find_by_name(name)
