2d3ad87eeca07e248c3de6ff6acc2bca3353468f
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-18 04:55:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,45 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
 
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+  end
+
+  def self.create
+  end
+
+  def self.new_from_db(row)
+    new_stu = self.new
+    new_stu.id = row[0]
+    new_stu.name = row[1]
+    new_stu.grade = row[2]
+  end
+
+  def self.find_by_name
+  end
+
+  def update
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
